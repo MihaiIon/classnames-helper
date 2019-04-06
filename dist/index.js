51deceab0da36bbbc648e164eefd9d5f49d59f01
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2019-04-06 16:38:09 -0400

    added helpers

--- dist/index.js
+++ dist/index.js
@@ -8,44 +8,71 @@ exports["default"] = _default;
 // ======================================================
 // Source File
 // ======================================================
-var is = require("is-type-of");
+// TypeCheck Helpers
+// ======================================================
+var isString = function isString(arg) {
+  return typeof arg === "string" || arg instanceof String;
+};
+
+var isArray = function isArray(arg) {
+  return arg instanceof Array || Array.isArray(arg);
+};
+
+var isBoolean = function isBoolean(arg) {
+  return typeof arg === "boolean";
+}; // Helpers
+// ======================================================
+
 /**
  * Returns the first element if the second element is "true", else "null".
- * @param {[string, boolean]} array
+ * @param {[String, Boolean], [String, String, Boolean]} array
  */
 
 
 var arrayHelper = function arrayHelper(array) {
-  if (array.length === 2 && isString(array[0])) return typeof array[1] === "boolean" && array[1] ? array[0] : null;
-  if (array.length > 2 && isString(array[0]) && isString(array[1]) && typeof array[2] === "boolean") return array[2] ? array[0] : array[1];
-  return null;
-};
+  switch (array.length) {
+    case 2:
+      return isString(array[0]) && isBoolean(array[1]) && array[1] ? array[0] : null;
+
+    case 3:
+      if (isString(array[0]) && isString(array[1]) && isBoolean(array[2])) return array[2] ? array[0] : array[1];
+
+    default:
+      return null;
+  }
+}; // Default export
+// ======================================================
+
 /**
- * Provides a string of classes based on the values of its arguments. This function
- * can take an unlimited number of arguments.
+ * Provides a string of classenames by joining each argument (with conditionnal checks).
+ * This function can take an unlimited number of arguments.
  *
  * Here is an example of a simple use case:
  *
  * // Import
- * import classNamesHelper from "path/to/classNamesHelper";
+ * import classNamesHelper from "classNamesHelper";
  *
  * // Usage
  * classNamesHelper(
  *   "c-component",
  *   "o-layout",
- *   ["-small", isSmall],
- *   ["-red", isRed],
+ *   ["-small", true],
+ *   ["-red", false],
+ *   ["-blue", "-green", true],
+ *   ["-yellow", "-orange", false],
  *   "-shrinked"
  * );
  *
- * @param {string | [string, boolean]} arguments
+ * // Ouput: "c-component o-layout -small -blue -orange -shrinked"
+ *
+ * @param {String | [String, Boolean] | [String, String, Boolean]} arguments
  */
 
 
 function _default() {
   var args = Array.prototype.slice.call(arguments);
   return args.length > 0 ? args.map(function (arg) {
-    if (arg instanceof Array) return arrayHelper(arg);
+    if (isArray(arg)) return arrayHelper(arg);
     if (isString(arg) && arg.length > 0) return arg;
     return null;
   }).filter(function (el) {
