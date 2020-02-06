2e1a8d5d579a7c326a91505ff739ee0c7ef814e8
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2020-02-05 19:06:16 -0500

    removed callback feature

--- src/__tests__/classnames.test.ts
+++ src/__tests__/classnames.test.ts
@@ -1,23 +1,8 @@
 // ======================================================
-// Classnames lib Test File
+// lib Tests File
 // ======================================================
 
 import cn from '..';
-
-// Helpers
-// ======================================================
-
-/**
- * Mock boolean functions.
- * @param b Returned value
- */
-const cond = (b: boolean): (() => boolean) => () => b;
-
-/**
- * Mock number functions.
- * @param n Returned value
- */
-const select = (n: number): (() => number) => () => n;
 
 // Test
 // ======================================================
@@ -94,32 +79,6 @@ test('Tuple (2): [boolean, string]', () => {
   expect(cn([true, 'class-1   class-2'])).toBe('class-1 class-2');
 });
 
-test('Tuple (2): [() => boolean, string]', () => {
-  // Empty
-  expect(cn([cond(true), ''])).toBe('');
-  expect(cn([cond(false), ''])).toBe('');
-  expect(cn([cond(true), ' '])).toBe('');
-  expect(cn([cond(false), ' '])).toBe('');
-  expect(cn([cond(true), '  '])).toBe('');
-  expect(cn([cond(false), '  '])).toBe('');
-  // Default
-  expect(cn([cond(true), 'conditional-class'])).toBe('conditional-class');
-  expect(cn([cond(false), 'conditional-class'])).toBe('');
-  // Trimming
-  expect(cn([cond(true), ' conditional-class '])).toBe('conditional-class');
-  expect(cn([cond(false), ' conditional-class '])).toBe('');
-  expect(cn([cond(true), '  conditional-class  '])).toBe('conditional-class');
-  expect(cn([cond(false), '  conditional-class  '])).toBe('');
-  // Multiple strings (default)
-  expect(cn([cond(true), 'class-1 class-2'])).toBe('class-1 class-2');
-  expect(cn([cond(false), 'class-1 class-2'])).toBe('');
-  // Multiple strings (trimming)
-  expect(cn([cond(true), ' class-1 class-2 '])).toBe('class-1 class-2');
-  expect(cn([cond(true), '  class-1 class-2  '])).toBe('class-1 class-2');
-  expect(cn([cond(true), 'class-1  class-2'])).toBe('class-1 class-2');
-  expect(cn([cond(true), 'class-1   class-2'])).toBe('class-1 class-2');
-});
-
 test('Tuple (3): [boolean, string, string]', () => {
   // Empty
   expect(cn([true, '', ''])).toBe('');
@@ -146,56 +105,30 @@ test('Tuple (3): [boolean, string, string]', () => {
   expect(cn([true, 'true-1   true-2', 'false-1   false-2'])).toBe('true-1 true-2');
 });
 
-test('Tuple (3): [() => boolean, string, string]', () => {
-  // Empty
-  expect(cn([cond(true), '', ''])).toBe('');
-  expect(cn([cond(false), '', ''])).toBe('');
-  expect(cn([cond(true), ' ', ' '])).toBe('');
-  expect(cn([cond(false), ' ', ' '])).toBe('');
-  expect(cn([cond(true), '  ', '  '])).toBe('');
-  expect(cn([cond(false), '  ', '  '])).toBe('');
-  // Default
-  expect(cn([cond(true), 'if-true', 'if-false'])).toBe('if-true');
-  expect(cn([cond(false), 'if-true', 'if-false'])).toBe('if-false');
-  // Trimming
-  expect(cn([cond(true), ' if-true ', ' if-false '])).toBe('if-true');
-  expect(cn([cond(false), ' if-true ', ' if-false '])).toBe('if-false');
-  expect(cn([cond(true), '  if-true  ', '  if-false  '])).toBe('if-true');
-  expect(cn([cond(false), '  if-true  ', '  if-false  '])).toBe('if-false');
-  // Multiple strings (default)
-  expect(cn([cond(true), 'true-1 true-2', 'false-1 false-2'])).toBe('true-1 true-2');
-  expect(cn([cond(false), 'true-1 true-2', 'false-1 false-2'])).toBe('false-1 false-2');
-  // Multiple strings (trimming)
-  expect(cn([cond(true), ' true-1 true-2 ', ' false-1 false-2 '])).toBe('true-1 true-2');
-  expect(cn([cond(true), '  true-1 true-2  ', '  false-1 false-2  '])).toBe('true-1 true-2');
-  expect(cn([cond(true), 'true-1  true-2', 'false-1  false-2'])).toBe('true-1 true-2');
-  expect(cn([cond(true), 'true-1   true-2', 'false-1   false-2'])).toBe('true-1 true-2');
-});
-
 test('Tuple (c): [() => number, ...string[]]', () => {
   // Empty
-  expect(cn([select(0), '', '', ''])).toBe('');
-  expect(cn([select(1), ' ', ' ', ' '])).toBe('');
-  expect(cn([select(2), '  ', '  ', '  '])).toBe('');
-  expect(cn([select(3), '  ', '  ', '  '])).toBe('');
+  expect(cn([0, '', '', ''])).toBe('');
+  expect(cn([1, ' ', ' ', ' '])).toBe('');
+  expect(cn([2, '  ', '  ', '  '])).toBe('');
+  expect(cn([3, '  ', '  ', '  '])).toBe('');
   // Default
-  expect(cn([select(0), 'class-1', 'class-2', 'class-3'])).toBe('class-1');
-  expect(cn([select(1), 'class-1', 'class-2', 'class-3'])).toBe('class-2');
-  expect(cn([select(2), 'class-1', 'class-2', 'class-3'])).toBe('class-3');
-  expect(cn([select(3), 'class-1', 'class-2', 'class-3'])).toBe('');
+  expect(cn([0, 'class-1', 'class-2', 'class-3'])).toBe('class-1');
+  expect(cn([1, 'class-1', 'class-2', 'class-3'])).toBe('class-2');
+  expect(cn([2, 'class-1', 'class-2', 'class-3'])).toBe('class-3');
+  expect(cn([3, 'class-1', 'class-2', 'class-3'])).toBe('');
   // Trimming
-  expect(cn([select(0), ' class-1 ', ' class-2 ', ' class-3 '])).toBe('class-1');
-  expect(cn([select(1), '  class-1  ', '  class-2  ', '  class-3  '])).toBe('class-2');
+  expect(cn([0, ' class-1 ', ' class-2 ', ' class-3 '])).toBe('class-1');
+  expect(cn([1, '  class-1  ', '  class-2  ', '  class-3  '])).toBe('class-2');
   // Multiple strings (default)
-  expect(cn([select(0), 'c1 c2', 'c3 c4', 'c5 c6'])).toBe('c1 c2');
-  expect(cn([select(1), 'c1 c2', 'c3 c4', 'c5 c6'])).toBe('c3 c4');
-  expect(cn([select(2), 'c1 c2', 'c3 c4', 'c5 c6'])).toBe('c5 c6');
-  expect(cn([select(3), 'c1 c2', 'c3 c4', 'c5 c6'])).toBe('');
+  expect(cn([0, 'c1 c2', 'c3 c4', 'c5 c6'])).toBe('c1 c2');
+  expect(cn([1, 'c1 c2', 'c3 c4', 'c5 c6'])).toBe('c3 c4');
+  expect(cn([2, 'c1 c2', 'c3 c4', 'c5 c6'])).toBe('c5 c6');
+  expect(cn([3, 'c1 c2', 'c3 c4', 'c5 c6'])).toBe('');
   // Multiple strings (trimming)
-  expect(cn([select(0), ' c1 c2 ', ' c3 c4 ', ' c5 c6 '])).toBe('c1 c2');
-  expect(cn([select(1), '  c1 c2  ', '  c3 c4  ', '  c5 c6  '])).toBe('c3 c4');
-  expect(cn([select(1), 'c1  c2', 'c3  c4', 'c5  c6'])).toBe('c3 c4');
-  expect(cn([select(2), 'c1   c2', 'c3   c4', 'c5   c6'])).toBe('c5 c6');
+  expect(cn([0, ' c1 c2 ', ' c3 c4 ', ' c5 c6 '])).toBe('c1 c2');
+  expect(cn([1, '  c1 c2  ', '  c3 c4  ', '  c5 c6  '])).toBe('c3 c4');
+  expect(cn([1, 'c1  c2', 'c3  c4', 'c5  c6'])).toBe('c3 c4');
+  expect(cn([2, 'c1   c2', 'c3   c4', 'c5   c6'])).toBe('c5 c6');
 });
 
 test('Multi-Type arguments: string & boolean conditions', () => {
@@ -227,45 +160,16 @@ test('Multi-Type arguments: string & boolean conditions', () => {
   ).toBe('a b c d e f i j o p qr s v w');
 });
 
-test('Multi-Type arguments: string & boolean functions', () => {
-  // Single class
-  expect(
-    cn(
-      'a',
-      'b',
-      [cond(true), ' c '],
-      [cond(false), ' d '],
-      [cond(true), '  e  ', '  f  '],
-      [cond(false), '  g', 'h  '],
-      'ij',
-      [cond(false), 'k', 'l'],
-    ),
-  ).toBe('a b c e h ij l');
-  // Multi-class
-  expect(
-    cn(
-      ' a b ',
-      '  c d  ',
-      [cond(true), 'e  f'],
-      [cond(false), 'g  h'],
-      [cond(true), 'i   j', 'k   l'],
-      [cond(false), 'm    n', 'o    p'],
-      'qr   s',
-      [cond(false), ' t u', 'v w '],
-    ),
-  ).toBe('a b c d e f i j o p qr s v w');
-});
-
-test('Multi-Type arguments: string & number functions', () => {
+test('Multi-Type arguments: string & number conditions', () => {
   // Single class
   expect(
     cn(
       'a',
       ' b ',
-      [select(0), '  c  ', '  d  ', '  e  '],
-      [select(1), '  c  ', '  d  ', '  e  '],
-      [select(2), '  c  ', '  d  ', '  e  '],
-      [select(3), '  c  ', '  d  ', '  e  '],
+      [0, '  c  ', '  d  ', '  e  '],
+      [1, '  c  ', '  d  ', '  e  '],
+      [2, '  c  ', '  d  ', '  e  '],
+      [3, '  c  ', '  d  ', '  e  '],
       'f',
     ),
   ).toBe('a b c d e f');
@@ -274,10 +178,10 @@ test('Multi-Type arguments: string & number functions', () => {
     cn(
       'a b',
       ' c d ',
-      [select(0), '  e f  ', 'g  h', 'i   j'],
-      [select(1), '  e f  ', 'g  h', 'i   j'],
-      [select(2), '  e f  ', 'g  h', 'i   j'],
-      [select(3), '  e f  ', 'g  h', 'i   j'],
+      [0, '  e f  ', 'g  h', 'i   j'],
+      [1, '  e f  ', 'g  h', 'i   j'],
+      [2, '  e f  ', 'g  h', 'i   j'],
+      [3, '  e f  ', 'g  h', 'i   j'],
       '  k   l  ',
     ),
   ).toBe('a b c d e f g h i j k l');
@@ -289,13 +193,13 @@ test('Multi-Type arguments: mixed', () => {
       'a',
       [true, 'b'],
       [false, 'c', 'd'],
-      [cond(true), 'e'],
-      [cond(false), 'f', 'g'],
-      [select(0), 'h', 'i', 'j'],
+      [true, 'e'],
+      [false, 'f', 'g'],
+      [0, 'h', 'i', 'j'],
       'k',
       [true, 'l', 'm'],
-      [select(10), 'n', 'o', 'p'],
-      [select(2), 'q', 'r', 's'],
+      [10, 'n', 'o', 'p'],
+      [2, 'q', 'r', 's'],
       [false, 't'],
     ),
   ).toBe('a b d e g h k l s');
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,6 @@
 // Source File
 // ======================================================
 
-// Types
-// ======================================================
-
-type ConditionOrBoolFunction = boolean | (() => boolean);
-
 // Helpers
 // ======================================================
 
@@ -61,15 +56,10 @@ const getStringFromArray = (arr: any[], index: number): string => {
  * Joins classnames with optional conditions while removing unnecessary spaces.
  */
 export default (
-  ...args: (
-    | string
-    | [ConditionOrBoolFunction, string]
-    | [ConditionOrBoolFunction, string, string]
-    | [() => number, ...string[]]
-  )[]
+  ...args: (string | [boolean, string] | [boolean, string, string] | [number, ...string[]])[]
 ): string => {
   let _str: string = '';
-  let _arr: [ConditionOrBoolFunction, ...string[]] | [() => number, ...string[]];
+  let _arr: [boolean, ...string[]] | [number, ...string[]];
 
   for (const arg of args) {
     // Append classname
@@ -85,31 +75,27 @@ export default (
         case 1:
           break;
 
-        // Tuple (2): [ConditionOrBoolFunction, string]
+        // Tuple (2): [boolean, string]
         case 2:
-          _arr = arg as [ConditionOrBoolFunction, string];
+          _arr = arg as [boolean, string];
           if (typeof _arr[0] === 'boolean') {
             _str += _arr[0] ? formatString(_arr[1] as string) : '';
-          } else if (typeof _arr[0] === 'function') {
-            _str += _arr[0]() ? formatString(_arr[1] as string) : '';
           }
           break;
 
-        // Tuple (3): [ConditionOrBoolFunction, string, string]
+        // Tuple (3): [boolean, string, string]
         case 3:
-          _arr = arg as [ConditionOrBoolFunction, string, string];
+          _arr = arg as [boolean, string, string];
           if (typeof _arr[0] === 'boolean') {
             _str += _arr[0] ? formatString(_arr[1] as string) : formatString(_arr[2] as string);
-          } else if (typeof _arr[0] === 'function') {
-            _str += _arr[0]() ? formatString(_arr[1] as string) : formatString(_arr[2] as string);
           }
           break;
 
-        // Tuple (x): [() => number), ...string[]]
+        // Tuple (x): [number), ...string[]]
         default:
-          _arr = arg as [() => number, ...string[]];
-          if (typeof _arr[0] === 'function') {
-            _str += formatString(getStringFromArray(_arr, _arr[0]()) as string);
+          _arr = arg as [number, ...string[]];
+          if (typeof _arr[0] === 'number') {
+            _str += formatString(getStringFromArray(_arr, _arr[0]) as string);
           }
       }
     }
